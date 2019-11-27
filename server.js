122b00c2a30e30fa2673ff381e84171633613055
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-11-26 20:20:13 -0600

    fix form

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger = require('morgan');
+const exphbs = require('express-handlebars');
 const path = require('path');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const port = process.env.PORT || 3000;
 
@@ -9,40 +11,28 @@ const port = process.env.PORT || 3000;
 const app = express();
 const projects = require('./data/project');
 const skills = require('./data/skills');
-// Use morgan and body parser with our app
-app.use(logger('dev'));
+
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
+app.use(bodyParser.json());
 
 // Make public a static dir
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Set Handlebars.
-const exphbs = require('express-handlebars');
-
-app.engine(
-  'handlebars',
-  exphbs({
-    defaultLayout: 'main',
-    partialsDir: path.join(__dirname, '/views/layouts/partials')
-  })
-);
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 /* SET ROUTES */
 app.get('/', (req, res) => {
-  //const projects = require('../data/projects.js');
-  //const websites = require('../data/websites.js');
-  res.render(
-    'index',
-    { projects: projects, skills: skills } /*, {
-    
-    websites: websites
-  }*/
-  );
+  res.render('index', { projects: projects, skills: skills });
+});
+
+app.post('/send', (req, res) => {
+  const name = req.body.name,
 });
 app.listen(port, function() {
   console.log('App running on port ' + port);
